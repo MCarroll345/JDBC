d057f503a7ee73c9441175db1fa18a471cf2353b
Mark Carroll <g00419267@atu.ie>
2023-11-07 13:32:05 +0000

    Loop finished

--- ArrayDemo/src/main/java/ie/atu/Main.java
+++ ArrayDemo/src/main/java/ie/atu/Main.java
@@ -5,15 +5,14 @@ public class Main {
         int[] numbers;
         int x,input;
 
-        numbers = new int[10]
+        numbers = new int[10];
 
         System.out.println("The output from this program is\n\n");
 
         for(x=0;x>9;x++){
             input = (x + 1) * 100;
             numbers[x] = input;
-            System.out.print("Element at index ");
-
+            System.out.print("Element at index " + x + ": " + input + "\n");
         }
     }
 }
