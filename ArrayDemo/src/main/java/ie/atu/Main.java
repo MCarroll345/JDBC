0c2954fccc3e0abd493c96f0bb562e6dec05f67b
Mark Carroll <g00419267@atu.ie>
2023-11-07 13:30:11 +0000

    Array initialized and for loop created

---
+++ ArrayDemo/src/main/java/ie/atu/Main.java
@@ -0,0 +1,19 @@
+package ie.atu;
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numbers;
+        int x,input;
+
+        numbers = new int[10]
+
+        System.out.println("The output from this program is\n\n");
+
+        for(x=0;x>9;x++){
+            input = (x + 1) * 100;
+            numbers[x] = input;
+            System.out.print("Element at index ");
+
+        }
+    }
+}
