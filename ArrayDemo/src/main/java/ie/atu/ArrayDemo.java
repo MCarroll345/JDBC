6aa25f641293f479f59f1d9c0a68bd649ba07e10
Mark Carroll <g00419267@atu.ie>
2023-11-07 14:12:07 +0000

    Moved and created files

---
+++ ArrayDemo/src/main/java/ie/atu/ArrayDemo.java
@@ -0,0 +1,27 @@
+package ie.atu;
+import java.util.Scanner;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        Scanner scanner  = new Scanner(System.in);
+
+        int[] numbers;
+        int x,input,uIn;
+
+        numbers = new int[10];
+
+        System.out.println("The output from this program is\n");
+
+        for(x=0;x<9;x++){
+            input = (x + 1) * 100;
+            numbers[x] = input;
+            System.out.print("Element at index " + x + ": " + input + "\n");
+        }
+
+        System.out.print("Please enter a value for index 9: ");
+        uIn = scanner.nextInt();
+        numbers[9] = uIn;
+        System.out.print("Element at index 9 : " + uIn + "\n");
+
+    }
+}
