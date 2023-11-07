e52b595cd5e094326315b7ad7aeec24d42f15a5c
Mark Carroll <g00419267@atu.ie>
2023-11-07 14:43:45 +0000

    Enhanced for loop created

--- ArrayDemo/src/main/java/ie/atu/ArryList.java
+++ ArrayDemo/src/main/java/ie/atu/ArryList.java
@@ -4,27 +4,24 @@ import java.util.Scanner;
 
 public class ArryList {
 
-    static void rayLoop( ArrayList<Integer> numbers){
-        for(Integer var : numbers){
-            System.out.print(var);
-        }
-    }
     public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<>();
 
-        Scanner scanner  = new Scanner(System.in);
+        //Scanner scanner  = new Scanner(System.in);
         int x,input,uIn;
-
-        System.out.println("The output from this program is\n");
-
 
         for(x=0;x<9;x++){
             input = (x + 1) * 100;
+
             numbers.add(input);
-            System.out.print("Element at index " + x + ": ");
-            rayLoop(numbers);
-            System.out.print("\n");
+
+
         }
+
+        for(int var : numbers){
+            System.out.print("Element at index " + x + ": " + var + "\n");
+        }
+        System.out.println("The output from this program is\n");
 
 
 
