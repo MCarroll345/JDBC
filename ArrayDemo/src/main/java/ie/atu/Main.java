7312654f280de047b4553dc4661e56fca74aa49b
Mark Carroll <g00419267@atu.ie>
2023-11-07 13:50:08 +0000

    Added user input

--- ArrayDemo/src/main/java/ie/atu/Main.java
+++ ArrayDemo/src/main/java/ie/atu/Main.java
@@ -1,18 +1,28 @@
 package ie.atu;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner  = new Scanner(System.in);
 
         int[] numbers;
-        int x,input;
+        int x,input,uIn;
 
         numbers = new int[10];
 
         System.out.println("The output from this program is\n");
 
-        for(x=0;x<10;x++){
+        for(x=0;x<9;x++){
             input = (x + 1) * 100;
             numbers[x] = input;
             System.out.print("Element at index " + x + ": " + input + "\n");
         }
+
+        System.out.print("Please enter a value for index 9: ");
+        uIn = scanner.nextInt();
+        numbers[10] = uIn;
+        System.out.print("Element at index 10 : " + uIn + "\n");
+
+
+
     }
 }
